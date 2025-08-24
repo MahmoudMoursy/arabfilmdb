adcb4c7fa7fa8a41fa6cdb91cd2d529380e88902
Abdalla343 <Abdallaahmed2268199@gmail.com>
2025-08-24 20:48:48 +0300

    @

--- src/App.jsx
+++ src/App.jsx
@@ -30,15 +30,15 @@ const ProtectedRoute = ({ children }) => {
 };
 
 function App() {
-  const { user } = useSelector(state => state.user);
-  const dispatch = useDispatch();
-  useEffect(() => {
-  const storedUser = localStorage.getItem("user");
-  if (storedUser) {
-    dispatch(setUser(JSON.parse(storedUser)));
-  }
-}, []);
-console.log(user);
+//   // const { user } = useSelector(state => state.user);
+//   // const dispatch = useDispatch();
+//   useEffect(() => {
+//   const storedUser = localStorage.getItem("user");
+//   if (storedUser) {
+//     dispatch(setUser(JSON.parse(storedUser)));
+//   }
+// }, []);
+// console.log(user);
 
 
   return (
